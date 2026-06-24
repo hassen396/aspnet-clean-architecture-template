10c3932f91e219117f6dbc224410735c23e8a60e
Hassen <smilex396>
2026-06-24 13:27:24 +0300

    add a user entity

---
+++ src/AuthForge.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthForge.Domain.Entities.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsActive { get; set; } = true;
+}
